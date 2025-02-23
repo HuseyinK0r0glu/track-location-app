bdd685f33263d735563e2b82b0b4d55ffc452944
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-23 11:00:11 +0300

    navigators added in App.js

--- frontend/tracks/App.js
+++ frontend/tracks/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+/// switch type of navigator used for switching between different screens without allowing users to go back
+import { createAppContainer , createSwitchNavigator } from 'react-navigation';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+import { createStackNavigator } from 'react-navigation-stack';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+
+
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDetailScreen from './src/screens/TrackDetailScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
+const switchNavigator = createSwitchNavigator({
+
+  // there will be parts here 
+  // first one is for authentication flow
+  // second one is non authenticaton flow that contains our tracks and etc.
+
+  // we are going to have links to other navigations 
+
+  loginFlow: createStackNavigator({
+    Signup : SignupScreen,
+    Signin : SigninScreen
+  }),
+  mainFlow: createMaterialBottomTabNavigator({
+    trackListFlow : createStackNavigator({
+      TrackList : TrackListScreen,
+      TrackDetail : TrackDetailScreen 
+    }), 
+    CreateTrack : TrackCreateScreen,
+    Account : AccountScreen,
+    
+  })
+
 });
+
+export default createAppContainer(switchNavigator);
