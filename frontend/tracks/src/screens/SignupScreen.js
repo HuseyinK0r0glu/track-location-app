f2f5bb37f340c96bd96202bf80e66f67266b89b7
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-23 12:15:59 +0300

    styling added

--- frontend/tracks/src/screens/SignupScreen.js
+++ frontend/tracks/src/screens/SignupScreen.js
@@ -1,11 +1,11 @@
 import React from "react";
-import {Text , StyleSheet} from "react-native";
+import {View ,  Text , StyleSheet} from "react-native";
 import {Input,Button} from "react-native-elements";
 import Spacer from "../components/Spacer"
 
 const SignupScreen = ({navigation}) => {
     return (
-        <>
+        <View style = {styles.container} >
             <Spacer>
                 <Text style = {styles.headerText} >Sign Up for Tracker</Text>
             </Spacer>
@@ -15,17 +15,29 @@ const SignupScreen = ({navigation}) => {
             <Spacer>
                 <Button title = "Sign Up"/> 
             </Spacer>    
-        </> 
+        </View> 
     );
 
 };
 
 const styles = StyleSheet.create({
     headerText: {
+        /// h3 for texts from react-native-elements was giving error so I used this one
         fontSize: 32,  // Similar to h3 size
         fontWeight: "bold",  // Similar to h3 style
         marginBottom: 20,  // Similar spacing to h3 in the original component
     },
+    container : {
+        flex : 1,
+        justifyContent : 'center',
+        marginBottom : 200
+    }
 });
+
+SignupScreen.navigationOptions = () => {
+    return {
+        headerShown: false,
+    };
+};
 
 export default SignupScreen;
