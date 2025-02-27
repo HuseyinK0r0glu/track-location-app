7312f10b231527ea0e488971f5d0338e7ca4e354
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-27 11:19:36 +0300

    coordination between contexts

--- frontend/tracks/src/components/TrackForm.js
+++ frontend/tracks/src/components/TrackForm.js
@@ -2,21 +2,33 @@ import React , {use, useContext} from 'react';
 import {Button , Input} from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext} from '../context/LocationContext';
+import useSaveTrack from '../hooks/useSaveTrack';
 
 const TrackForm = () => {
 
     const { state : {name , recording , locations} , startRecording , stopRecording , changeName } = useContext(LocationContext);
 
+    const [saveTrack] = useSaveTrack();
+
     return (
         <>
             <Spacer>
-                <Input value = {name} onChangeText = {() => changeName()} placeholder='Enter Name'/>
+                <Input value = {name} onChangeText = {(newName) => changeName(newName)} placeholder='Enter Name'/>
             </Spacer>
-            {recording 
-                ? <Button title = "Stop" onPress={stopRecording}/>
-                : <Button title = "Start Recording" onPress={startRecording}/>     
-            }
+            <Spacer>
+                {recording 
+                    ? <Button title = "Stop" onPress={stopRecording}/>
+                    : <Button title = "Start Recording" onPress={startRecording}/>     
+                }
+            </Spacer>
             
+            <Spacer>    
+                { !recording && locations.length ? (
+                    <Button title = "Save Recording" onPress={saveTrack}/>
+                ) : null
+                }
+            </Spacer>
+
         </>
     );
 };
