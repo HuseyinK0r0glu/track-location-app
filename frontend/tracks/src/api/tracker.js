5ca0ec529849e3babfe5491b1c9988ef790d33f1
HuseyinK0r0glu <korogluh27@gmail.com>
2025-02-28 10:41:14 +0300

    automatic authentication

--- frontend/tracks/src/api/tracker.js
+++ frontend/tracks/src/api/tracker.js
@@ -1,5 +1,23 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default axios.create({
-    baseURL : 'https://b1ce-193-140-239-246.ngrok-free.app'
+const instance = axios.create({
+    /// baseURL changes every 8 hours or whenever restarts 
+    baseURL : 'https://65bf-193-140-239-246.ngrok-free.app'
 });
+
+instance.interceptors.request.use(
+    // config is the configuration for the request
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if(token){
+            config.headers.Authorization = `Bearer ${token}`
+        }
+        return config; // This is implicitly wrapped in a Promise.resolve(config)
+    },
+    (err) => {
+        return Promise.reject(err);
+    }
+);
+
+export default instance;
